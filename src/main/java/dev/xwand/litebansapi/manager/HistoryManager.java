1315a2f735edbae8efb58f1976464da498dcb34a
PorkSkulls <gabrielvidal1372@gmail.com>
2022-07-10 16:52:31 -0400

    Converted HistoryManager and PunishmentType to accept the new Punishment.java object.

--- src/main/java/dev/xwand/litebansapi/manager/HistoryManager.java
+++ src/main/java/dev/xwand/litebansapi/manager/HistoryManager.java
@@ -1,14 +1,12 @@
 package dev.xwand.litebansapi.manager;
 
-import dev.xwand.litebansapi.LitebansAPI;
 import dev.xwand.litebansapi.punishment.Punishment;
 import litebans.api.Database;
 
-import javax.xml.crypto.Data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -16,40 +14,26 @@ import java.util.concurrent.CompletableFuture;
 /**
  * @author xWand
  */
-public class HistoryManager {
 
-    private Database database;
+public final class HistoryManager {
 
-    public HistoryManager(Database database) {
+    private final Database database;
+
+    public HistoryManager(final Database database) {
         this.database = database;
     }
 
-    public List<Punishment> getBans(UUID uuid) {
-        List<Punishment> bans = new ArrayList<>();
-        String query = "SELECT * FROM {bans} WHERE uuid=?";
-        Database database = this.database;
+    public List<Punishment> getBans(final UUID uuid) {
+
+        final List<Punishment> bans = new LinkedList<>();
+        final String query = "SELECT * FROM {bans} WHERE uuid=?";
 
         CompletableFuture.runAsync(() -> {
-            try (PreparedStatement st = database.prepareStatement(query)) {
+            try (PreparedStatement st = this.database.prepareStatement(query)) {
                 st.setString(1, uuid.toString());
-                try (ResultSet rs = st.executeQuery()) {
+                try (final ResultSet rs = st.executeQuery()) {
                     while (rs.next()) {
-                        bans.add(new Punishment(
-                                rs.getLong("id"),
-                                rs.getString("uuid"),
-                                rs.getString("ip"),
-                                rs.getString("reason"),
-                                rs.getString("banned_by_uuid"),
-                                rs.getString("banned_by_name"),
-                                rs.getLong("time"),
-                                rs.getLong("until"),
-                                rs.getShort("template"),
-                                rs.getString("server_scope"),
-                                rs.getString("server_origin"),
-                                rs.getBoolean("silent"),
-                                rs.getBoolean("ipban"),
-                                rs.getBoolean("ipban_wildcard"),
-                                rs.getBoolean("active")));
+                        bans.add(new Punishment(rs));
                     }
                 }
             } catch (SQLException e) {
